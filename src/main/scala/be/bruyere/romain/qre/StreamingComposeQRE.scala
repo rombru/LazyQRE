f576a89ca8c86fa7ef8aacca8d93918c587eaf37
rombru <romain.bruyere@student.unamur.be>
2021-12-11 07:48:42 +0100

    Rename StreamingCompositionQRE.scala to StreamingComposeQRE.scala

--- src/main/scala/be/bruyere/romain/qre/StreamingComposeQRE.scala
+++ src/main/scala/be/bruyere/romain/qre/StreamingComposeQRE.scala
@@ -2,7 +2,7 @@ package be.bruyere.romain.qre
 
 import be.bruyere.romain.eval.{Eval, StreamingCompose}
 
-case class StreamingCompositionQRE[In, Out, ChildOut](child1: QRE[In, ChildOut], child2: QRE[ChildOut, Out]) extends QRE[In, Out] {
+case class StreamingComposeQRE[In, Out, ChildOut](child1: QRE[In, ChildOut], child2: QRE[ChildOut, Out]) extends QRE[In, Out] {
 
   protected[qre] override def create[Fn](): Eval[In, Out, Fn] = {
     StreamingCompose[In, Out, ChildOut, Fn](child1.create(), child2.create(), None)
